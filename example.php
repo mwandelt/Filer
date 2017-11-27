b231eb47e17b9c91b30b64f0e10bdf7409af719d
Martin Wandelt <martin@virthos.net>
2017-11-27 10:31:49 +0100

    Fixed file name in example code

--- example.php
+++ example.php
@@ -6,7 +6,7 @@ $filer->lock();
 sleep(5);
 $filer->write( 'example_data.php', 'This is my first data file' );
 sleep(5);
-var_dump( $filer->read('content.php') );
+var_dump( $filer->read('example_data.php') );
 $filer->unlock();
 
 
